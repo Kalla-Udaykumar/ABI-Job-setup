fcc05d59cc9b8c162839f703920adfce6d7ca859
Uday <102657313+Kalla-Udaykumar@users.noreply.github.com>
2023-10-03 12:02:38 +0530

    Update tep_build.sh

--- 32381/idf/tep_build.sh
+++ 32381/idf/tep_build.sh
@@ -5,9 +5,9 @@ export no_proxy=intel.com,.intel.com,10.0.0.0/8,192.168.0.0/16,localhost,.local,
 pip3 install ruamel.yaml
 pip3 install kas==2.5.0
 cd ${WORKSPACE}/abi/meta-tep-build-setup
-./generate_release.sh tep-container
-./generate_release.sh tep-acrn
-./generate_release.sh tep-kvm
+--preserve-env=https_proxy,http_proxy,no_proxy ./generate_release.sh tep-container
+--preserve-env=https_proxy,http_proxy,no_proxy ./generate_release.sh tep-acrn
+--preserve-env=https_proxy,http_proxy,no_proxy ./generate_release.sh tep-kvm
 mkdir -p ${WORKSPACE}/abi/upload 
 bzip2 -z  tep-container-release.tar.gz 
 bzip2 -z  tep-acrn-release.tar.gz 
