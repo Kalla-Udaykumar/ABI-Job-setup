1be85017798a57b4b9b98307c0ba1efea5343f8c
Uday <102657313+Kalla-Udaykumar@users.noreply.github.com>
2023-10-03 11:21:09 +0530

    Create tep_build.sh

---
+++ 32381/idf/tep_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -xe
+pip3 install kas==2.5.0
+cd ${WORKSPACE}/abi/meta-tep-build-setup
+./generate_release.sh tep-container
+./generate_release.sh tep-acrn
+./generate_release.sh tep-kvm
+mkdir -p ${WORKSPACE}/abi/upload 
+bzip2 -z  tep-container-release.tar.gz 
+bzip2 -z  tep-acrn-release.tar.gz 
+bzip2 -z  tep-kvm-release.tar.gz 
+cp *.bz2 ${WORKSPACE}/abi/upload
+                
