f24a8667669599c8ac21c2ab34da996e01beeac9
Uday <102657313+Kalla-Udaykumar@users.noreply.github.com>
2023-10-04 13:22:59 +0530

    Update tep_build.sh

--- 32381/idf/tep_build.sh
+++ 32381/idf/tep_build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -xe
-export http_proxy=http://proxy-dmz.intel.com:912
-export https_proxy=http://proxy-dmz.intel.com:912
-export no_proxy=intel.com,.intel.com,10.0.0.0/8,192.168.0.0/16,localhost,.local,127.0.0.0/8,134.134.0.0/16
+#export http_proxy=http://proxy-dmz.intel.com:912
+#export https_proxy=http://proxy-dmz.intel.com:912
+#export no_proxy=intel.com,.intel.com,10.0.0.0/8,192.168.0.0/16,localhost,.local,127.0.0.0/8,134.134.0.0/16
 pip3 install ruamel.yaml
 pip3 install kas==2.5.0
 cd ${WORKSPACE}/abi/meta-tep-build-setup
---preserve-env=https_proxy,http_proxy,no_proxy ./generate_release.sh tep-container
---preserve-env=https_proxy,http_proxy,no_proxy ./generate_release.sh tep-acrn
---preserve-env=https_proxy,http_proxy,no_proxy ./generate_release.sh tep-kvm
+./generate_release.sh tep-container
+#--preserve-env=https_proxy,http_proxy,no_proxy 
+./generate_release.sh tep-acrn
+./generate_release.sh tep-kvm
 mkdir -p ${WORKSPACE}/abi/upload 
 bzip2 -z  tep-container-release.tar.gz 
 bzip2 -z  tep-acrn-release.tar.gz 
